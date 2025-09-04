2d9298621cae4cde846dafd22163c6f7d086c8b3
btx0424 <btx0424@outlook.com>
2025-09-04 15:07:54 +0800

    fix

--- active_adaptation/envs/mdp/rewards/reward_gait.py
+++ active_adaptation/envs/mdp/rewards/reward_gait.py
@@ -18,21 +18,20 @@ class max_feet_height(Reward):
         self.target_height = target_height
 
         self.max_height = torch.zeros(self.num_envs, len(self.body_ids), device=self.device)
-    
+        self.rew = torch.zeros(self.num_envs, 1, device=self.device)
+
     def reset(self, env_ids):
         self.max_height[env_ids] = 0.
     
     def update(self):
         feet_height = self.asset.data.body_pos_w[:, self.body_ids, 2]
         in_contact = self.contact_sensor.data.current_contact_time[:, self.body_contact_ids] > 0.0
-        self.max_height = torch.where(
-            in_contact,
-            0.,
-            torch.maximum(self.max_height, feet_height)
-        )
+        self.max_height = torch.maximum(self.max_height, feet_height)
+        self.rew = self.max_height.clamp_max(self.target_height)
+        self.max_height = torch.where(in_contact, 0., self.max_height)
 
     def compute(self) -> torch.Tensor:
         first_contact = self.contact_sensor.compute_first_contact(self.env.step_dt)[:, self.body_contact_ids]
-        rew = self.max_height.clamp_max(self.target_height) * first_contact
+        rew = self.rew * first_contact
         return rew.sum(1, keepdim=True)
 
