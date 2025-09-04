501a610c1f3d1d18f0d9818f27740a907c8f5bc2
btx0424 <btx0424@outlook.com>
2025-09-04 19:39:29 +0800

    jpos interval

--- active_adaptation/envs/mdp/observations/obs_joint.py
+++ active_adaptation/envs/mdp/observations/obs_joint.py
@@ -51,3 +51,108 @@ class joint_vel(Observation):
         transform = joint_space_symmetry(self.asset, self.joint_names)
         return transform
 
+
+class joint_pos_multistep(Observation):
+    def __init__(
+        self,
+        env,
+        joint_names: str=".*",
+        steps: int=4, 
+        interval: int=1,
+        noise_std: float=0.,
+    ):
+        super().__init__(env)
+        self.steps = steps
+        self.interval = interval
+        self.noise_std = max(noise_std, 0.)
+        self.asset: Articulation = self.env.scene["robot"]
+        self.joint_ids, self.joint_names = self.asset.find_joints(joint_names)
+        self.num_joints = len(self.joint_ids)
+
+        shape = (self.num_envs, steps, self.num_joints)
+        self.joint_pos_multistep = torch.zeros(shape, device=self.device)
+        self.joint_pos = torch.zeros(self.num_envs, 2, self.num_joints, device=self.device)
+    
+    def post_step(self, substep):
+        self.joint_pos[:, substep % 2] = self.asset.data.joint_pos[:, self.joint_ids]
+    
+    def update(self):
+        next_joint_pos_multistep = self.joint_pos_multistep.roll(1, 1)
+        next_joint_pos = self.joint_pos.mean(1)
+        next_joint_pos_multistep[:, 0] = next_joint_pos
+        self.joint_pos_multistep = torch.where(
+            (self.env.episode_length_buf % self.interval == 0).reshape(self.num_envs, 1, 1),
+            next_joint_pos_multistep,
+            self.joint_pos_multistep
+        )
+    
+    def compute(self):
+        joint_pos = self.joint_pos_multistep.clone()
+        if self.noise_std > 0:
+            joint_pos = random_noise(joint_pos, self.noise_std)
+        return joint_pos.reshape(self.num_envs, -1)
+    
+    def symmetry_transforms(self):
+        transform = joint_space_symmetry(self.asset, self.joint_names)
+        return transform.repeat(self.steps)
+
+
+class joint_vel_multistep(Observation):
+    def __init__(
+        self,
+        env,
+        joint_names=".*",
+        steps: int=4,
+        noise_std: float=0.,
+    ):
+        super().__init__(env)
+        self.steps = steps
+        self.noise_std_max = max(noise_std, 0.)
+        self.from_pos = True
+        self.asset: Articulation = self.env.scene["robot"]
+        self.joint_ids, self.joint_names = self.asset.find_joints(joint_names)
+        self.num_joints = len(self.joint_ids)
+
+        shape = (self.num_envs, steps, self.num_joints)
+        
+        self.joint_vel_multistep = torch.zeros(shape, device=self.device)
+        
+        self.noise_std = torch.zeros(self.num_envs, self.num_joints, device=self.device)
+        if self.from_pos:
+            shape = (self.num_envs, self.env.decimation, self.num_joints)
+            self.joint_pos_substep = torch.zeros(shape, device=self.device)
+        else:
+            shape = (self.num_envs, 2, self.num_joints)
+            self.joint_vel_substep = torch.zeros(shape, device=self.device)
+    
+    def reset(self, env_ids: torch.Tensor):
+        self.noise_std[env_ids] = torch.rand(len(env_ids), self.num_joints, device=self.device) * self.noise_std_max
+
+    def post_step(self, substep):
+        if self.from_pos:
+            self.joint_pos_substep[:, substep] = self.asset.data.joint_pos[:, self.joint_ids]
+        else:
+            self.joint_vel_substep[:, substep % 2] = self.asset.data.joint_vel[:, self.joint_ids]
+    
+    def update(self):
+        self.joint_vel_multistep = self.joint_vel_multistep.roll(1, 1)
+        if self.from_pos:
+            joint_vel = self.joint_pos_substep.diff(dim=1).mean(dim=1) / self.env.physics_dt
+        else:
+            joint_vel = self.joint_vel_substep.mean(dim=1)
+        self.joint_vel_multistep[:, 0] = joint_vel
+    
+    def compute(self):
+        joint_vel = self.joint_vel_multistep.clone()
+        joint_vel = random_noise(joint_vel, self.noise_std)
+        return joint_vel.reshape(self.num_envs, -1)
+
+    def symmetry_transforms(self):
+        transform = joint_space_symmetry(self.asset, self.joint_names)
+        return transform.repeat(self.steps)
+
+
+def random_noise(x: torch.Tensor, std: float):
+    return x + torch.randn_like(x).clamp(-3., 3.) * std
+
+
