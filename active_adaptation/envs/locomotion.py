dc58ce2a8ac8bdcc87c00d6db470be81613f80b3
btx0424 <btx0424@outlook.com>
2026-01-17 21:14:16 +0800

    amend

--- active_adaptation/envs/locomotion.py
+++ active_adaptation/envs/locomotion.py
@@ -58,7 +58,7 @@ class SimpleEnvIsaac(_EnvBase):
         
         registry = Registry.instance()
         
-        scene_cfg = InteractiveSceneCfg(num_envs=self.cfg.num_envs, env_spacing=2.5)
+        scene_cfg = InteractiveSceneCfg(num_envs=self.cfg.num_envs, env_spacing=2.5, replicate_physics=False)
         scene_cfg.sky_light = AssetBaseCfg(
             prim_path="/World/skyLight",
             spawn=sim_utils.DomeLightCfg(
