5b2406705ca396c6c9f80f530bc8218dea9934e9
btx0424 <btx0424@outlook.com>
2026-02-05 20:15:34 +0800

    algo cleanup

--- active_adaptation/learning/ppo/ppo_go2.py
+++ active_adaptation/learning/ppo/ppo_go2.py
@@ -30,7 +30,6 @@ import functools
 
 from torchrl.data import CompositeSpec, TensorSpec
 from torchrl.modules import ProbabilisticActor
-from torchrl.envs.transforms import VecNorm
 from tensordict import TensorDict
 from tensordict.nn import (
     TensorDictModuleBase, 
@@ -40,16 +39,12 @@ from tensordict.nn import (
 
 from hydra.core.config_store import ConfigStore
 from dataclasses import dataclass, field
-from typing import Union, List
+from typing import Union, Tuple
 from collections import OrderedDict
 
-from ..utils.valuenorm import ValueNorm1, ValueNormFake
-from ..modules.distributions import IndependentNormal
-from .common import *
-
-
-
-CMD_KEY = "command_"
+from active_adaptation.learning.modules import VecNorm
+from active_adaptation.learning.modules.distributions import IndependentNormal
+from active_adaptation.learning.ppo.common import *
 
 
 @torch.no_grad()
@@ -73,7 +68,7 @@ class PPOConfig:
     value_norm: bool = False
 
     checkpoint_path: Union[str, None] = None
-    in_keys: List[str] = field(default_factory=lambda: [CMD_KEY, OBS_KEY, OBS_PRIV_KEY, "height_scan"])
+    in_keys: Tuple[str, ...] = (OBS_KEY, "extero")
 
 
 cs = ConfigStore.instance()
@@ -137,10 +132,11 @@ class PPOPolicy(TensorDictModuleBase):
         observation_spec: CompositeSpec, 
         action_spec: CompositeSpec, 
         reward_spec: TensorSpec,
-        device
+        device,
+        env=None,
     ):
         super().__init__()
-        self.cfg = cfg
+        self.cfg = PPOConfig(**cfg)
         self.device = device
 
         self.entropy_coef = self.cfg.entropy_coef
@@ -150,26 +146,28 @@ class PPOPolicy(TensorDictModuleBase):
         self.action_dim = action_spec.shape[-1]
         self.gae = GAE(0.99, 0.95)
         
-        if cfg.value_norm:
-            value_norm_cls = ValueNorm1
-        else:
-            value_norm_cls = ValueNormFake
-        self.value_norm = value_norm_cls(input_shape=1).to(self.device)
-
         fake_input = observation_spec.zero()
-        self.vecnorm = VecNorm(
-            in_keys=[OBS_KEY, OBS_PRIV_KEY, "height_scan"],
-            shapes=[fake_input[OBS_KEY].shape[-1],
-                    fake_input[OBS_PRIV_KEY].shape[-1],
-                    fake_input["height_scan"].shape[-2:]]
+        self.vecnorm_proprio = VecNorm(
+            input_shape=observation_spec[OBS_KEY].shape[-1:],
+            stats_shape=observation_spec[OBS_KEY].shape[-1:],
+            decay=1.0
         ).to(self.device)
-        self.vecnorm(fake_input)
+
+        self.vecnorm_extero = VecNorm(
+            input_shape=observation_spec["extero"].shape[-2:],
+            stats_shape=observation_spec["extero"].shape[-2:],
+            decay=1.0
+        ).to(self.device)
+
+        self.vecnorm = Seq(
+            Mod(self.vecnorm_proprio, [OBS_KEY], ["policy_normed"]),
+            Mod(self.vecnorm_extero, ["extero"], ["extero_normed"]),
+        ).to(self.device)
         
         _actor = nn.Sequential(ResidualFC(256), Actor(self.action_dim))
         self.actor_encoder = MixedEncoder()
         actor_module = Seq(
-            CatTensors([CMD_KEY, OBS_KEY, OBS_PRIV_KEY], "mlp_inp", sort=False),
-            Mod(self.actor_encoder, ["mlp_inp", "height_scan", "cnn_mask"], ["actor_feature"]),
+            Mod(self.actor_encoder, ["policy_normed", "extero_normed", "cnn_mask"], ["actor_feature"]),
             Mod(_actor, ["actor_feature"], ["loc", "scale"]),
             Mod(nn.LazyLinear(1), ["actor_feature"], ["aux_pred"])
         )
@@ -184,11 +182,11 @@ class PPOPolicy(TensorDictModuleBase):
         _critic = nn.Sequential(ResidualFC(256), nn.LazyLinear(1))
         self.critic_encoder = MixedEncoder()
         self.critic = Seq(
-            CatTensors([CMD_KEY, OBS_KEY, OBS_PRIV_KEY], "mlp_inp", sort=False),
-            Mod(self.critic_encoder, ["mlp_inp", "height_scan", "cnn_mask"], ["critic_feature"]),
+            Mod(self.critic_encoder, ["policy_normed", "extero_normed", "cnn_mask"], ["critic_feature"]),
             Mod(_critic, ["critic_feature"], ["state_value"])
         ).to(self.device)
 
+        self.vecnorm(fake_input)
         self.actor(fake_input)
         self.critic(fake_input)
 
@@ -211,21 +209,24 @@ class PPOPolicy(TensorDictModuleBase):
         self.actor.apply(init_)
         self.critic.apply(init_)
     
-    def get_rollout_policy(self, mode: str="train"):
-        if mode == "train":
-            vecnorm = self.vecnorm
+    def get_rollout_policy(self, mode: str="train", critic: bool = False):
+        if critic:
+            policy = Seq(self.vecnorm, self.actor, self.critic)
         else:
-            vecnorm = self.vecnorm.to_observation_norm()
-        policy = Seq(vecnorm, self.actor)
+            policy = Seq(self.vecnorm, self.actor)
         return policy
+    
+    def on_stage_start(self, stage: str):
+        pass
 
-    # @torch.compile
+    @VecNorm.freeze()
     def train_op(self, tensordict: TensorDict):
+        assert VecNorm.FROZEN, "VecNorm must be frozen before training"
 
-        tensordict = tensordict.copy()
+        tensordict = tensordict.exclude("stats")
         infos = []
 
-        self._compute_advantage(tensordict, self.critic, "adv", "ret", update_value_norm=True)
+        self.compute_advantage(tensordict, self.critic, "adv", "ret")
         tensordict["adv"] = normalize(tensordict["adv"], subtract_mean=True)
 
         for epoch in range(self.cfg.ppo_epochs):
@@ -248,20 +249,23 @@ class PPOPolicy(TensorDictModuleBase):
         return dict(sorted(infos.items()))
 
     @torch.no_grad()
-    def _compute_advantage(
+    def compute_value(self, tensordict: TensorDict):
+        self.vecnorm(tensordict)
+        return self.critic(tensordict)
+    
+    @torch.no_grad()
+    def compute_advantage(
         self, 
         tensordict: TensorDict,
         critic: Mod, 
         adv_key: str="adv",
         ret_key: str="ret",
-        update_value_norm: bool=True,
     ):
-        self.vecnorm.freeze()
-        with tensordict.view(-1) as tensordict_flat:
-            critic(tensordict_flat)
-            self.vecnorm(tensordict_flat["next"])
-            critic(tensordict_flat["next"])
-        self.vecnorm.unfreeze()
+        keys = tensordict.keys(True, True)
+        if not ("state_value" in keys and ("next", "state_value") in keys):
+            with tensordict.view(-1) as tensordict_flat:
+                critic(self.vecnorm(tensordict_flat))
+                critic(self.vecnorm(tensordict_flat["next"]))
 
         values = tensordict["state_value"]
         next_values = tensordict["next", "state_value"]
@@ -270,13 +274,8 @@ class PPOPolicy(TensorDictModuleBase):
         terms = tensordict[TERM_KEY]
         dones = tensordict[DONE_KEY]
         discount = tensordict["next", "discount"]
-        values = self.value_norm.denormalize(values)
-        next_values = self.value_norm.denormalize(next_values)
 
         adv, ret = self.gae(rewards, terms, dones, values, next_values, discount)
-        if update_value_norm:
-            self.value_norm.update(ret)
-        ret = self.value_norm.normalize(ret)
 
         tensordict.set(adv_key, adv)
         tensordict.set(ret_key, ret)
@@ -284,12 +283,13 @@ class PPOPolicy(TensorDictModuleBase):
 
     # @torch.compile
     def _update(self, tensordict: TensorDict):
+        self.vecnorm(tensordict)
         dist = self.actor.get_dist(tensordict)
         log_probs = dist.log_prob(tensordict[ACTION_KEY])
         entropy = dist.entropy().mean()
 
         adv = tensordict["adv"]
-        log_ratio = (log_probs - tensordict["sample_log_prob"]).unsqueeze(-1)
+        log_ratio = (log_probs - tensordict["action_log_prob"]).unsqueeze(-1)
         ratio = torch.exp(log_ratio)
         surr1 = adv * ratio
         surr2 = adv * ratio.clamp(1.-self.clip_param, 1.+self.clip_param)
@@ -300,10 +300,8 @@ class PPOPolicy(TensorDictModuleBase):
         values = self.critic(tensordict)["state_value"]
         value_loss = self.critic_loss_fn(b_returns, values)
         value_loss = (value_loss * (~tensordict["is_init"])).mean()
-
-        # aux_loss = F.mse_loss(tensordict["aux_pred"], b_returns)
         
-        loss = policy_loss + entropy_loss + value_loss # + 1.0 * aux_loss
+        loss = policy_loss + entropy_loss + value_loss
         self.opt.zero_grad()
         loss.backward()
         actor_grad_norm = nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
@@ -344,117 +342,4 @@ class PPOPolicy(TensorDictModuleBase):
                 failed_keys.append(name)
         print(f"Successfully loaded {succeed_keys}.")
         return failed_keys
-    
-    def learn(self, env, cfg):
-        import os
-        import wandb
-        import logging
 
-        from active_adaptation.utils.torchrl import SyncDataCollector
-        from tqdm import tqdm
-        from omegaconf import OmegaConf
-
-        run = wandb.init(
-            job_type=cfg.wandb.job_type,
-            entity=cfg.wandb.entity,
-            project=cfg.wandb.project,
-            mode=cfg.wandb.mode,
-            tags=cfg.wandb.tags,
-        )
-        run.config.update(OmegaConf.to_container(cfg))
-
-        frames_per_batch = self.cfg.train_every * env.num_envs
-        total_iters = int(cfg.total_frames / frames_per_batch)
-        save_interval = cfg.save_interval
-        log_interval = (env.max_episode_length // cfg.algo.train_every) + 1
-        collector = SyncDataCollector(
-            env,
-            policy=self.get_rollout_policy("train"),
-            frames_per_batch=frames_per_batch,
-            total_frames=cfg.total_frames,
-            device=cfg.sim.device,
-            return_same_td=True,
-        )
-
-        pbar = tqdm(collector, total=total_iters)
-
-        stats_keys = [
-            k for k in env.reward_spec.keys(True, True) 
-            if isinstance(k, tuple) and k[0] == "stats"
-        ]
-        episode_stats = EpisodeStats(stats_keys)
-
-        ckpt_path = None
-        for i, data in enumerate(pbar):
-            info = {}
-            episode_stats.add(data)
-
-            if i % log_interval == 0 and len(episode_stats):
-                for k, v in sorted(episode_stats.pop().items(True, True)):
-                    key = "train/" + ("/".join(k) if isinstance(k, tuple) else k)
-                    info[key] = torch.mean(v.float()).item()
-                info.update(env.extra)
-            
-            info.update(self.train_op(data))
-            
-            if save_interval > 0  and i % save_interval == 0:
-                checkpoint_name = f"checkpoint_{i}"
-                ckpt_path = os.path.join(run.dir, f"{checkpoint_name}.pt")
-                state_dict = self.state_dict()
-                torch.save(state_dict, ckpt_path)
-                logging.info(f"Saved checkpoint to {str(ckpt_path)}")
-            
-            info["env_frames"] = collector._frames
-            run.log(info)
-            
-            print(OmegaConf.to_yaml(info))
-            table_print(env.stats_ema)
-            
-            if ckpt_path is not None:
-                print(f"Latest checkpoint path: {ckpt_path}")
-
-
-from prettytable import PrettyTable
-def table_print(info):
-    pt = PrettyTable()
-    nrow = max(len(v) for v in info.values())
-    for k, v in info.items():
-        data = [f"{kk}:{vv:.3f}" for kk, vv in v.items()]
-        data += [" "] * (nrow - len(data))
-        pt.add_column(k, data)
-    print(pt)
-
-
-from typing import Sequence
-class EpisodeStats:
-    def __init__(self, in_keys: Sequence[str] = None):
-        self.in_keys = in_keys
-        self._stats = []
-        self._episodes = 0
-
-    def add(self, tensordict: TensorDictBase) -> TensorDictBase:
-        next_tensordict = tensordict["next"]
-        done = next_tensordict["done"]
-        if done.any():
-            done = done.squeeze(-1)
-            self._episodes += done.sum().item()
-            next_tensordict = next_tensordict.select(*self.in_keys)
-            self._stats.extend(
-                next_tensordict[done].clone().unbind(0)
-            )
-        return len(self)
-    
-    def pop(self):
-        stats: TensorDictBase = torch.stack(self._stats)
-        self._stats.clear()
-        return stats
-
-    def __len__(self):
-        return len(self._stats)
-
-
-def normalize(x: torch.Tensor, subtract_mean: bool=False):
-    if subtract_mean:
-        return (x - x.mean()) / x.std().clamp(1e-7)
-    else:
-        return x  / x.std().clamp(1e-7)
