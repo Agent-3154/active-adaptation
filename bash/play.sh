cc05628d1d58e773bc6ffeae9301d899fd3e2b36
Elgce <elgceben@gmail.com>
2026-03-08 16:15:29 +0800

    torso grav

--- bash/play.sh
+++ bash/play.sh
@@ -7,8 +7,7 @@ PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 cd "$PROJECT_DIR"
 
 python scripts/eval_run.py \
-    -r elgceben/g1_room/x5x85jf8 \
-    -i 3000 \
+    -r elgceben/g1_room/mqmfzt6c \
     -p \
     --lights \
     --vis-rgb \
