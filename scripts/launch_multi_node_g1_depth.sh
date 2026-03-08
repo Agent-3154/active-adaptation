3b33eeb5832f905d4822e02671f1befa9a14a304
Elgce <elgceben@gmail.com>
2026-03-09 00:23:13 +0800

    add da3

---
+++ scripts/launch_multi_node_g1_depth.sh
@@ -0,0 +1,28 @@
+source /root/miniconda3/bin/activate gsloco
+
+cd /cpfs/user/benqingwei/gsloco/IsaacLab/ && ./isaaclab.sh -i
+cd /cpfs/user/benqingwei/gsloco/active-adaptation/ && pip install -e .
+cd /cpfs/user/benqingwei/gsloco/gs-scene/ && pip install -e .
+cd /cpfs/user/benqingwei/gsloco/sirius-wheel-learning/ && pip install -e .
+
+pip install depth-anything-3
+
+sync && sleep 5
+
+cd /cpfs/user/benqingwei/gsloco/active-adaptation/scripts/
+
+export OMP_NUM_THREADS=4
+
+export http_proxy="https://benqingwei:M1u1Nzw0MrAzBSD4RSGv6uFTqzPCTpYfDzOSip3tmEEPGU00HhKErL9JpJHH@aliyun-proxy.pjlab.org.cn:13128"
+export https_proxy="https://benqingwei:M1u1Nzw0MrAzBSD4RSGv6uFTqzPCTpYfDzOSip3tmEEPGU00HhKErL9JpJHH@aliyun-proxy.pjlab.org.cn:13128"
+export HTTP_PROXY="https://benqingwei:M1u1Nzw0MrAzBSD4RSGv6uFTqzPCTpYfDzOSip3tmEEPGU00HhKErL9JpJHH@aliyun-proxy.pjlab.org.cn:13128"
+export HTTPS_PROXY="https://benqingwei:M1u1Nzw0MrAzBSD4RSGv6uFTqzPCTpYfDzOSip3tmEEPGU00HhKErL9JpJHH@aliyun-proxy.pjlab.org.cn:13128"
+
+export WANDB_API_KEY=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
+
+
+torchrun --nnodes=${WORLD_SIZE} --nproc_per_node=8 --node_rank=${RANK} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} train_ppo.py \
+  task=G1AilabRoomNavDepth task.num_envs=512 \
+  wandb.project=g1_room wandb.run_name=G1AilabRoomNavDepth \
+  algo=ppo_geoloco headless=True total_frames=10_000_000_000 \
+  task.scene.point_lights=false task.command.vis_waypoints=false
