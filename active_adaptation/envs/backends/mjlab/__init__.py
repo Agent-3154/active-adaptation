37d25f3cff5315bca41a057d7c4c69d8e68646ea
Haoyang Weng <elijahgalahad@gmail.com>
2026-03-14 02:00:29 +0800

    update

--- active_adaptation/envs/backends/mjlab/__init__.py
+++ active_adaptation/envs/backends/mjlab/__init__.py
@@ -1,10 +1,8 @@
 from .adapter import MjlabSceneAdapter, MjlabSimAdapter
 from .env import MjlabBackendEnv
-from .viewer import MjLabViewer
 
 __all__ = [
     "MjlabSimAdapter",
     "MjlabSceneAdapter",
     "MjlabBackendEnv",
-    "MjLabViewer",
 ]
