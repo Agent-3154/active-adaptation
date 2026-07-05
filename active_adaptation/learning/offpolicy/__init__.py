11447f4c259fce1b917e9fd9c0fa40e603417285
btx0424 <btx0424@outlook.com>
2026-07-05 18:56:40 +0800

    sac1 and train_offpolicy

--- active_adaptation/learning/offpolicy/__init__.py
+++ active_adaptation/learning/offpolicy/__init__.py
@@ -1,5 +1,7 @@
-from .sac import SAC
+import os
+import importlib
 
-__all__ = [
-    "SAC",
-]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+for file in os.listdir(dir_path):
+    if file.endswith(".py") and not file.startswith("_"):
+        importlib.import_module(f".{file[:-3]}", __package__)
